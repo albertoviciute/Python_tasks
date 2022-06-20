fd5db6e655d87d26be39198a932bbcdcd0cf5fac
albertoviciute <albertoviciutee@gmail.com>
2022-06-20 22:09:40 +0300

    New

--- Task_3/main.py
+++ Task_3/main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+# Turimas "audi" dict.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Parašykite funkciją "show_object_values", kuri kaip argumentą priims objektą
+# ir grąžins visus jo "values" list'e.
 
+audi = {
+  "make": 'audi',
+  "model": 'A6',
+  "year": 2005,
+  "color": 'white',
+}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def show_object_values(car):
+    car_info = list(audi.values())
+    print(car_info)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+show_object_values(audi)
